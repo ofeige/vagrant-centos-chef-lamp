1e4aa188ab6483e9da5e074cf027c3e9e98a41c6
Oliver Feige <oliver@feige.net>
2014-12-30 22:54:58 +0100

    update for chef 12 compatibility

--- chef/cookbooks/lamp/metadata.rb
+++ chef/cookbooks/lamp/metadata.rb
@@ -1,9 +1,10 @@
+name             "lamp"
 maintainer       "Oliver Feige"
 maintainer_email "ofeige@gmx.de"
 license          "All rights reserved"
 description      "Installs/Configures a LAMP Stack"
 #long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.2"
 
 depends "nginx"
 #depends "database"
