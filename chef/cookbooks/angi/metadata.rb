3c3aafdf77fb952fc2dc09e33c5fb2e530d070bc
Oliver Feige <oliver@feige.net>
2015-06-22 08:34:45 +0200

    remove angi cookbook

--- chef/cookbooks/angi/metadata.rb
+++
@@ -1,9 +0,0 @@
-name             "angi"
-maintainer       "Oliver Feige"
-maintainer_email "ofeige@gmx.de"
-license          "All rights reserved"
-description      "set up angi vhosts"
-version          "1.0"
-
-depends "nginx"
-
