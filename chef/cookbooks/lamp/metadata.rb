dd983dab7b6f073b640f2070df2a5744b002bb9e
Oliver Feige <oliver@feige.net>
2015-06-22 08:38:17 +0200

    cleanup lamp cookbook

--- chef/cookbooks/lamp/metadata.rb
+++ chef/cookbooks/lamp/metadata.rb
@@ -1,10 +1,10 @@
-name             "lamp"
-maintainer       "Oliver Feige"
-maintainer_email "ofeige@gmx.de"
-license          "All rights reserved"
-description      "Installs/Configures a LAMP Stack"
-#long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.2"
+name               "lamp"
+maintainer         "Oliver Feige"
+maintainer_email   "ofeige@gmx.de"
+license            "All rights reserved"
+description        "Installs/Configures a LAMP Stack"
+#long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version            "0.0.3"
 
-depends "nginx"
-#depends "database"
+depends 'mysql'
+depends 'nginx'
--- chef/cookbooks/lamp/recipes/default.rb
+++ chef/cookbooks/lamp/recipes/default.rb
@@ -1,25 +1,4 @@
-#include_recipe "apt" if platform?("ubuntu", "debian")
-#include_recipe "yum" if platform_family?("rhel")
-#include_recipe "mysql::client"
-#include_recipe "mysql::server"
-#include_recipe "database::mysql"
-#include_recipe "apache2"
-#include_recipe "apache2::mod_php5"
-#include_recipe "apache2::mod_rewrite"
-#include_recipe "apache2::mod_deflate"
-#include_recipe "apache2::mod_headers"
 include_recipe "nginx"
-
-include_recipe "iptables::disabled"
-
-
-# add the EPEL repo
-yum_repository 'epel' do
-  description 'Extra Packages for Enterprise Linux'
-  mirrorlist 'https://mirrors.fedoraproject.org/metalink?repo=epel-7&arch=$basearch'
-  gpgkey 'http://ftp.nluug.nl/pub/os/Linux/distr/fedora-epel/RPM-GPG-KEY-EPEL-7'
-  action :create
-end
 
 # add the remi repo
 yum_repository 'remi' do
@@ -37,152 +16,47 @@ yum_repository 'remi-php56' do
   action :create
 end
 
-%w( vim-enhanced mc less screen lynx curl telnet psmisc nano ).each do |prog|
-	package prog do
-		action :install
-	end
-end
-
-%w( php php-mcrypt php-gd php-xml php-ldap php-mysql php-mysqlnd php-mbstring php-pdo php-opcache php-devel ).each do |prog|
-#%w( php56 php56-php-mcrypt php56-php-gd php56-php-xml php56-php-ldap php56-php-mysql php56-php-mysqlnd php56-php-mbstring php56-php-pdo php56-php-opcache ).each do |prog|
-  package prog do
+%w(php php-mcrypt php-gd php-mysqlnd php-mbstring php-pdo php-opcache composer).each do |prog|
+    package prog do
     options "--enablerepo=remi,remi-php56"
     action :install
   end
 end
 
-#mysql_service 'default' do
-#      allow_remote_root true
-#      remove_anonymous_users true
-#      remove_test_database true
-#      server_root_password 'test'
-#      action :create
-#end
 
-#mysql_connection_info = {
-#  :host     => 'localhost',
-#  :username => 'root',
-#  :password => 'test'
-#}
-
-#mysql_database 'symphony' do
-#  connection mysql_connection_info
-#  action :create
-#end
-
-#or import from a dump file
-#mysql_database "myapp" do
-#  connection mysql_connection_info
-#  sql { ::File.open('/vagrant/data/mysql.schema.sql').read }
-#  action :query
-#end
-
-#database_user 'myapp_user' do
-#  connection mysql_connection_info
-#  password   'super_secret'
-#  provider   Chef::Provider::Database::MysqlUser
-#  action     :create
-#end
-
-#mysql_database_user 'myapp_user' do
-#  connection    mysql_connection_info
-#  password      'super_secret'
-#  database_name 'myapp'
-#  host          '%'
-#  privileges    [:all]
-#  action        :grant
-#end
-
-#web_app "opf_testapp" do
-#  server_name "localhost"
-#  server_aliases ["localhost"]
-#  docroot "/vagrant/web"
-#end
-
-file "/etc/php.d/99-display_errors.ini" do
-  owner "root"
-  group "root"
-  mode "0644"
-  action :create
-  content "display_errors = On"
-#  notifies :restart, "service[apache2]", :delayed
+mysql_service 'foo' do
+  port '3306'
+  version '5.7'
+  initial_root_password 'changeme'
+  action [:create, :start]
 end
 
-file "/etc/php.d/99-default_timezone.ini" do
-  owner "root"
-  group "root"
-  mode "0644"
-  action :create
-  content "date.timezone = 'Europe/Berlin'"
-#  notifies :restart, "service[apache2]", :delayed
-end
 
-file "/etc/profile.d/vialias.sh" do
-	owner "root"
-	group "root"
-	mode "0644"
-	action :create
-	content "alias vi=vim"
-end
+node['lamp']['vhosts'].each do |vhost|
 
-file "root/.vimrc" do
-	owner "root"
-	group "root"
-	mode "0644"
-	content "syntax on
-set noai
-set ts=4
-colorscheme desert"
-	action :create
-end
+	root = "#{node['nginx']['default_root']}/#{vhost}"
+	root_without_vhost = "#{node['nginx']['default_root']}"
+	template 'wildcard-site' do
 
-#directory "/var/www/nginx-default/" do
-#	owner "root"
-#	group "root"
-#	mode "0755"
-#	action :create
-#end
-#
-#execute "auto start php-fpm" do
-#    command "chkconfig php-fpm on"
-#end
+   	variables(
+  		:root => root,
+  		:root_without_vhost => root_without_vhost,
+  		:vhost => vhost
+  	)
 
-#service 'php-fpm' do
-#  supports :status => true, :restart => true, :reload => true
-#  action   :start
-#end
+    path "/etc/nginx/sites-available/#{vhost}"
+        action :create
+        source 'wildcard-site.erb'
+        owner 'root'
+        group 'root'
+        mode '0644'
+    end
 
-template 'default-site' do
-  path "/etc/nginx/sites-available/default"
-  action :create
-  source 'default-site.erb'
-  owner 'root'
-  group 'root'
-  mode '0644'
-  notifies :reload, 'service[nginx]', :delayed
-end
+    link "/etc/nginx/sites-enabled/#{vhost}" do
+        to "/etc/nginx/sites-available/#{vhost}"
+        notifies :reload, 'service[nginx]', :delayed
+    end
 
-template 'dot.my.cnf' do
-	path "/root/.my.cnf"
-	action :create
-  owner 'root'
-  group 'root'
-  mode '0644'
-end
-
-template 'dot.my.cnf' do
-	path "/home/vagrant/.my.cnf"
-	action :create
-  owner 'root'
-  group 'root'
-  mode '0644'
-end
-
-php_fpm_pool "www" do
-   listen '/var/run/php-fpm-www.sock'
-   process_manager "dynamic"
-   max_requests 5000
-   php_options 'php_admin_flag[log_errors]' => 'on', 'php_admin_value[memory_limit]' => '32M'
 end
 
 directory "/var/lib/nginx" do
@@ -197,4 +71,11 @@ directory "/var/lib/nginx/tmp" do
    group 'root'
    mode '0777'
    action :create
+end
+
+php_fpm_pool "www" do
+   listen '/var/run/php-fpm-www.sock'
+   process_manager "dynamic"
+   max_requests 5000
+   php_options 'php_admin_flag[log_errors]' => 'on', 'php_admin_value[memory_limit]' => '32M'
 end
