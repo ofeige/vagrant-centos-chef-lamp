ea06cd917813b10e42a56be84aa69ff4ee403849
Oliver Feige <oliver@feige.net>
2016-08-10 10:44:33 +0200

    update version of lamp package

--- chef/cookbooks/lamp/metadata.rb
+++ chef/cookbooks/lamp/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email   "ofeige@gmx.de"
 license            "All rights reserved"
 description        "Installs/Configures a LAMP Stack"
 #long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version            "0.0.3"
+version            "0.0.4"
 
 depends 'mysql'
 depends 'nginx'
